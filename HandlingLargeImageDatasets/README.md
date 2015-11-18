38d0bbe7c96ee5bc1f3a03d4e72da97f7686fea5
Alejandro Cartas <alejandro.cartas@gmail.com>
2015-11-18 12:27:11 +0100

    LIDHL created

---
+++ HandlingLargeImageDatasets/README.md
@@ -0,0 +1,32 @@
+# Handling large images datasets
+
+The problem with handling directories containing a large number of files, specially images, is their size. For instance, Apple's Preview runs out of memory and crashes trying to show all the images. In this case, is better to use a simpler image viewer like [feh](http://feh.finalrewind.org/).
+
+## Large Image Dataset Handler Library (LIDHL)
+
+This is Bash shell util library for handling large image datasets.
+
+### Extracting all frames from a video
+
+```bash
+video_filepath="example.avi"
+num_padding_zeros=`extract_frames "$video_filepath"`
+```
+
+### Remove frames by range
+
+```bash
+rm_frames "example" "jpg" 11159 11290 "$num_padding_zeros"
+```
+
+### Massively list files in a directory
+
+```bash
+mass_ls -lah "example*.jpg"
+```
+
+### Massively move files in a directory
+
+```bash
+mass_mv "example*.jpg" dest_directory
+```
---
+++ HandlingLargeImageDatasets/lidhl.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#
+# Large Image Datasets Handler Library
+# Written by Alejandro Cartas.
+#
+
+LIDHL_VERSION=0.1
+
+# Returns the number of frames from a video
+function number_of_frames {
+	video_filepath="$1"
+    echo `ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_read_frames -of default=nokey=1:noprint_wrappers=1 "$video_filepath"`
+}
+
+# Returns the number of left padding zeros wrt. the number of frames
+function padding_zeros {
+	video_filepath="$1"
+	num_frames=`number_of_frames "$video_filepath"`
+	echo "${#num_frames}"
+}
+
+# Returns the number of frames per second from a video
+function frames_per_second {
+	video_filepath="$1"
+    echo `ffmpeg -i "$video_filepath" 2>&1 | sed -n "s/.*, \(.*\) fp.*/\1/p"`
+}
+
+# Extracts the frames from a video adding the precise number of padding zeros
+function extract_frames {	
+
+	video_filepath="$1"
+
+	if [ -z "$2" ]; then
+		num_frames=`number_of_frames $video_filepath`
+		num_padding_zeros="${#num_frames}"
+	else
+		num_padding_zeros="$2"  
+	fi
+
+    fps=`frames_per_second $video_filepath`
+	filename="${video_filepath%.*}"
+
+    ffmpeg -i $video_filepath -y -an -qscale 0 -r $fps/1 "$filename"_%0"$num_padding_zeros"d.jpg >&2
+
+    echo $num_padding_zeros
+}
+
+# Removes the frames by range
+function rm_frames {
+	
+	basename="$1"
+	file_extension="$2"
+	start_frame="$3"
+	end_frame="$4"
+	num_padding_zeros="$5"
+
+    for i in $(seq -f "%0""$num_padding_zeros""g" "$start_frame" "$end_frame")
+    do
+        rm "$basename""$i"."$file_extension"
+    done
+}
+
+# Massive files move
+function mass_mv {
+	filename_regex="$1"
+	new_directory="$2"
+	find . -type f -name "$filename_regex" | xargs -I '{}' mv {} "$new_directory"
+}
+
+# Massive list files
+function mass_ls {
+	ls_args="$1";
+	filename_regex="$2";
+	find . -type f -name "$filename_regex" | xargs ls "$ls_args";
+}

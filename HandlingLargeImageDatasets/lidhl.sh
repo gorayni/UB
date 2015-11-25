70b47183d8a7cbcad5b29f5d6c67700473f3b420
Alejandro Cartas <alejandro.cartas@gmail.com>
2015-11-25 12:33:36 +0100

    Large file downloader in chunks function added

--- HandlingLargeImageDatasets/lidhl.sh
+++ HandlingLargeImageDatasets/lidhl.sh
@@ -73,3 +73,56 @@ function mass_ls {
 	filename_regex="$2";
 	find . -type f -name "$filename_regex" | xargs ls "$ls_args";
 }
+
+# Returns the number of bytes of the files provided by the arg
+function number_of_bytes { wc -c $1 | tail -1 | sed -e 's/^[ \t]*//' | cut -d ' ' -f 1; }
+
+# Returns the size in bytes of a download 
+function download_filesize { curl --head $1 2> /dev/null | grep Content-Length | cut -d ' ' -f 2 | tr -d $'\r'; }
+
+# Downloads a large file in chunks of max. 4 GB
+function chunkie_download {
+	FOUR_GB=4294967296
+
+	URL=$1
+	filename=$2
+	if [ -z "$3" ]; then
+		chunk_size=$FOUR_GB
+	else
+		chunk_size="$3"
+	fi
+
+	num_files=`ls -1 "$filename".part* 2> /dev/null | wc -l | tail -1 | sed -e 's/^[ \t]*//'`
+	if [ "$num_files" -gt 0 ]; then
+	    part_number=`ls -1 "$filename".part* | sed -e "s/""$filename"".part//g" | sort -nr | head -1`
+		part_number=`expr $part_number + 1`
+
+		num_bytes=`number_of_bytes "$filename".part"*"`
+		num_bytes=`expr $num_bytes`	
+	else
+	    part_number=0
+	    num_bytes=0   
+	fi
+
+	total_filesize=`download_filesize $URL`
+
+	if [ "$total_filesize" -lt "$chunk_size" ]; then
+		curl -o  "$filename" "$URL"
+		return;	
+	fi
+
+	while [  $num_bytes -lt $total_filesize ]; do
+		curl --range "$num_bytes"-`expr $num_bytes + $chunk_size` -o  "$filename".part"$part_number" "$URL"
+
+		num_bytes=`number_of_bytes "$filename".part"*"`
+		num_bytes=`expr $num_bytes`
+
+		part_number=`expr $part_number + 1`
+	done
+}
+
+function join_chunks {
+	filename=$1
+	ls -1 "$filename".part* | sed 's/\([0-9]\)/;\1/' | sort -n -t\; -k2,2 | tr -d ';'
+	cat `ls -1 "$filename".part* | sed 's/\([0-9]\)/;\1/' | sort -n -t\; -k2,2 | tr -d ';'` > $filename;
+}

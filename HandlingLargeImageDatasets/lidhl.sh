a85b428ee40a2da0563417fa5c962c1aee7dc03d
Alejandro Cartas <gorayni@users.noreply.github.com>
2016-12-12 13:25:47 +0100

    Remove video to images functions

--- HandlingLargeImageDatasets/lidhl.sh
+++ HandlingLargeImageDatasets/lidhl.sh
@@ -6,45 +6,6 @@
 
 LIDHL_VERSION=0.1
 
-# Returns the number of frames from a video
-function number_of_frames {
-	video_filepath="$1"
-    echo `ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_read_frames -of default=nokey=1:noprint_wrappers=1 "$video_filepath"`
-}
-
-# Returns the number of left padding zeros wrt. the number of frames
-function padding_zeros {
-	video_filepath="$1"
-	num_frames=`number_of_frames "$video_filepath"`
-	echo "${#num_frames}"
-}
-
-# Returns the number of frames per second from a video
-function frames_per_second {
-	video_filepath="$1"
-    echo `ffmpeg -i "$video_filepath" 2>&1 | sed -n "s/.*, \(.*\) fp.*/\1/p"`
-}
-
-# Extracts the frames from a video adding the precise number of padding zeros
-function extract_frames {	
-
-	video_filepath="$1"
-
-	if [ -z "$2" ]; then
-		num_frames=`number_of_frames $video_filepath`
-		num_padding_zeros="${#num_frames}"
-	else
-		num_padding_zeros="$2"  
-	fi
-
-    fps=`frames_per_second $video_filepath`
-	filename="${video_filepath%.*}"
-
-    ffmpeg -i $video_filepath -y -an -qscale 0 -r $fps/1 "$filename"_%0"$num_padding_zeros"d.jpg >&2
-
-    echo $num_padding_zeros
-}
-
 # Removes the frames by range
 function rm_frames {
 	
